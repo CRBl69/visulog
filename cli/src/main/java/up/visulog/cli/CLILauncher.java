fb1708a81f88d9b17b524e76357d4820b8e28dce
Philippe Hinault <philippehinault45@gmail.com>
2021-09-29 19:29:41 +0200

    Resolution de l'issue crisan/visulog#2
    Ajout de la librairie JCommander pour faciliter
    l'ajout d'arguments de ligne de commande

--- cli/src/main/java/up/visulog/cli/CLILauncher.java
+++ cli/src/main/java/up/visulog/cli/CLILauncher.java
@@ -5,9 +5,40 @@ import up.visulog.analyzer.CountCommitsPerAuthorPlugin;
 import up.visulog.config.Configuration;
 import up.visulog.config.PluginConfig;
 
-import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Optional;
+import java.util.List;
+import java.util.ArrayList;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+
+class Arguments {
+    @Parameter(description = "Git path")
+    private String gitPath = ".";
+
+    @Parameter(names = { "-p", "--addPlugin" }, description = "Selectionner un plugin a ajouter")
+    private List<String> plugins;
+
+    @Parameter(names = { "-c", "--loadConfigFile" }, description = "Charger un fichier de configuration")
+    private String configFile;
+
+    @Parameter(names = { "-s", "--justSaveConfigFile" }, description = "Charger un fichier de configuration")
+    private boolean justSave;
+
+    public List<String> getPlugins() {
+        if(this.plugins != null)
+            return new ArrayList<String>(this.plugins);
+        else
+            return new ArrayList<String>();
+    }
+
+    public String getGitPath() {
+        return this.gitPath;
+    }
+}
 
 public class CLILauncher {
 
@@ -21,40 +52,21 @@ public class CLILauncher {
     }
 
     static Optional<Configuration> makeConfigFromCommandLineArgs(String[] args) {
-        var gitPath = FileSystems.getDefault().getPath(".");
         var plugins = new HashMap<String, PluginConfig>();
-        for (var arg : args) {
-            if (arg.startsWith("--")) {
-                String[] parts = arg.split("=");
-                if (parts.length != 2) return Optional.empty();
-                else {
-                    String pName = parts[0];
-                    String pValue = parts[1];
-                    switch (pName) {
-                        case "--addPlugin":
-                            // TODO: parse argument and make an instance of PluginConfig
-
-                            // Let's just trivially do this, before the TODO is fixed:
-
-                            if (pValue.equals("countCommits")) {
-                                plugins.put(CountCommitsPerAuthorPlugin.name, new PluginConfig().addChart("bars"));
-                            }
-                            break;
-                        case "--loadConfigFile":
-                            // TODO (load options from a file)
-                            break;
-                        case "--justSaveConfigFile":
-                            // TODO (save command line options to a file instead of running the analysis)
-                            break;
-                        default:
-                            return Optional.empty();
-                    }
-                }
-            } else {
-                gitPath = FileSystems.getDefault().getPath(arg);
+        var arguments = new Arguments();
+        JCommander.newBuilder()
+            .addObject(arguments)
+            .build()
+            .parse(args);
+        for (var plugin : arguments.getPlugins()) {
+            switch (plugin) {
+                case "countCommits":
+                    plugins.put(CountCommitsPerAuthorPlugin.name, new PluginConfig().addChart("bars"));
+                    break;
             }
         }
-        return Optional.of(new Configuration(gitPath, plugins));
+        Path path = Paths.get(arguments.getGitPath());
+        return Optional.of(new Configuration(path, plugins));
     }
 
     private static void displayHelpAndExit() {
--- cli/src/test/java/up/visulog/cli/TestCLILauncher.java
+++ cli/src/test/java/up/visulog/cli/TestCLILauncher.java
@@ -2,7 +2,6 @@ package up.visulog.cli;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class TestCLILauncher {
@@ -13,11 +12,8 @@ public class TestCLILauncher {
      */
     @Test
     public void testArgumentParser() {
-        var config1 = CLILauncher.makeConfigFromCommandLineArgs(new String[]{".", "--addPlugin=countCommits"});
+        var config1 = CLILauncher.makeConfigFromCommandLineArgs(new String[]{".", "--addPlugin", "countCommits"});
         assertTrue(config1.isPresent());
-        var config2 = CLILauncher.makeConfigFromCommandLineArgs(new String[] {
-            "--nonExistingOption"
-        });
-        assertFalse(config2.isPresent());
+        assertTrue(config1.get().getPluginConfigs().keySet().contains("countCommits"));
     }
 }
